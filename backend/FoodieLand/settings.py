03192d6c731ac097559d900e0cdc0e73800c3bac
WalidDeveloperCom <walidoffice46@gmail.com>
2025-08-16 13:19:07 +0600

    Demo email SMTP setting update

--- backend/FoodieLand/settings.py
+++ backend/FoodieLand/settings.py
@@ -134,4 +134,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#DEFAULT_FROM_EMAIL = "no-reply@yourdomain.com"
+#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+#EMAIL_HOST = "smtp.gmail.com"  # or your provider
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#EMAIL_HOST_USER = "your-email@gmail.com"
+#EMAIL_HOST_PASSWORD = "your-app-password"
