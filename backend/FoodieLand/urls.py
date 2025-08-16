95b7785d52a0bc4f20701d84cd84a9078f2cbfb2
WalidDeveloperCom <walidoffice46@gmail.com>
2025-08-16 12:55:12 +0600

    Setting and URLS update

--- backend/FoodieLand/urls.py
+++ backend/FoodieLand/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
 ]
 # Serve static and media files in development
 if settings.DEBUG:
